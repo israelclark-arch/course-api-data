0970475d3940ef95927cc76f9a462806436cc960
Israel Clark <clarkisraelofficial@gmail.com>
2021-02-19 12:46:49 +0100

    one to many mapping for topics and courses

--- src/main/java/io/sedettechnologies/courseapidata/course/CourseRepository.java
+++ src/main/java/io/sedettechnologies/courseapidata/course/CourseRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CourseRepository extends CrudRepository<Course, String> {
 
+//	methods strictly following the default naming convention of the inbuilt crud repository methods 
+//	will be implemented for us
 	public List<Course> findByTopicId(String topicId);
 	
 }
